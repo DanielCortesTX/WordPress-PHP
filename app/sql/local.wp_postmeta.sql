3b5ed36e50914b8120715fc6f0181b0b9d0ee2b1
Daniel Cortes <d5t5c5@yahoo.com>
2019-03-28 18:28:46 -0500

    feat: Added main page theme

--- app/sql/local.wp_postmeta.sql
+++ app/sql/local.wp_postmeta.sql
@@ -3,4 +3,11 @@
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_postmeta` VALUES
 (1,2,"_wp_page_template","default"),
-(2,3,"_wp_page_template","default");
+(2,3,"_wp_page_template","default"),
+(3,5,"_edit_lock","1553705399:1"),
+(4,6,"_edit_last","1"),
+(5,6,"_edit_lock","1553705487:1"),
+(8,9,"_edit_last","1"),
+(9,9,"_edit_lock","1553705736:1"),
+(12,11,"_edit_last","1"),
+(13,11,"_edit_lock","1553707379:1");
