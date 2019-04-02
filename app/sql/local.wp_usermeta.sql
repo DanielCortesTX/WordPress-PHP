3cadc4b19710a80e3e5101f06585f1f6aa019717
Daniel Cortes <d5t5c5@yahoo.com>
2019-04-01 21:22:32 -0500

    feat: Added custom queries

--- app/sql/local.wp_usermeta.sql
+++ app/sql/local.wp_usermeta.sql
@@ -2,10 +2,10 @@
 /*!40014 SET FOREIGN_KEY_CHECKS=0*/;
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_usermeta` VALUES
-(1,1,"nickname","danCor"),
+(1,1,"nickname","Schyenindogu"),
 (2,1,"first_name",""),
 (3,1,"last_name",""),
-(4,1,"description",""),
+(4,1,"description","A dashing rogue"),
 (5,1,"rich_editing","true"),
 (6,1,"syntax_highlighting","true"),
 (7,1,"comment_shortcuts","false"),
@@ -18,5 +18,8 @@ INSERT INTO `wp_usermeta` VALUES
 (14,1,"dismissed_wp_pointers","wp496_privacy"),
 (15,1,"show_welcome_panel","1"),
 (16,1,"session_tokens","a:1:{s:64:\"c56a420d78ca926b256e0b795b3c9c92922ee3e048de0089c8f854c8d441953a\";a:4:{s:10:\"expiration\";i:1554687450;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:114:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36\";s:5:\"login\";i:1553477850;}}"),
-(17,1,"wp_dashboard_quick_press_last_post_id","4"),
-(18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}");
+(17,1,"wp_dashboard_quick_press_last_post_id","38"),
+(18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
+(19,1,"managenav-menuscolumnshidden","a:5:{i:0;s:11:\"link-target\";i:1;s:11:\"css-classes\";i:2;s:3:\"xfn\";i:3;s:11:\"description\";i:4;s:15:\"title-attribute\";}"),
+(20,1,"metaboxhidden_nav-menus","a:1:{i:0;s:12:\"add-post_tag\";}"),
+(21,1,"nav_menu_recently_edited","4");
--- app/sql/local.wp_users.sql
+++ app/sql/local.wp_users.sql
@@ -2,4 +2,4 @@
 /*!40014 SET FOREIGN_KEY_CHECKS=0*/;
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_users` VALUES
-(1,"danCor","$P$B6JCY.cFUqZPaYKDsFV4aHG/FeYhjD0","dancor","dev-email@flywheel.local","","2019-03-25 01:35:14","",0,"danCor");
+(1,"danCor","$P$B6JCY.cFUqZPaYKDsFV4aHG/FeYhjD0","dancor","dev-email@flywheel.local","","2019-03-25 01:35:14","",0,"Schyenindogu");
